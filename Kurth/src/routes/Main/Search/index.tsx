92b979fc7dd0d5f53aff085837dd6ce4476651b5
Deploy PC <g.andrade0222@gmail.com>
2024-07-13 21:21:30 -0300

    v0.0.6.1 Search route uptdate

--- Kurth/src/routes/Main/Search/index.tsx
+++ Kurth/src/routes/Main/Search/index.tsx
@@ -2,14 +2,23 @@ import "./style.scss";
 import SearchBar from "../../../components/SearchBar";
 import * as User from "../../../constants/user";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Search() {
+  const [user, setUser] = useState<UserDTO[]>([]);
+  useEffect(() => {
+    User.findAll().then((response) => {
+      console.log(response.data.content);
+      setUser(response.data.content);
+    });
+  }, []);
+
   return (
     <div className="container-search">
       <SearchBar />
 
       <div className="wrapper-search">
-        {User.findAll().map((user) => (
+        {user && user.map((user =>
           <div key={user.id} className="user-icon">
             <Link to={`/profile/${user.username}`}>
               <img src={user.avatar} alt={user.username} className="icon" />
